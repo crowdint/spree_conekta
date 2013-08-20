e363dcded180cda17b58fbe9027caccb584ea42e
netmask <jonathan@devmask.net>
2013-08-20 12:37:49 -0500

    clean assets directory

--- spec/controllers/spree/conekta/payments_controller_spec.rb
+++ spec/controllers/spree/conekta/payments_controller_spec.rb
@@ -4,53 +4,34 @@ describe Spree::Conekta::PaymentsController do
   routes { Spree::Core::Engine.routes }
 
   let(:conekta_response){
-    {"_id"=>"521266d9ab94e9122e0000e4",
-     "data"=>{
-         "object"=>{
-             "id"=>"521266d8ab94e9122e0000e3",
-             "livemode"=>"false",
-             "created_at"=>"1376937689",
-             "status"=>"pending_payment",
-             "currency"=>"MXN",
-             "description"=>"R987654321-JG2EM438",
-             "reference_id"=>"R987654321-JG2EM438",
-             "failure_code"=>"",
-             "failure_message"=>"",
-             "amount"=>"10002",
-             "processed_at"=>"",
-             "fee"=>"231",
-             "customer"=>{
-                 "name"=>"Jonathan Garay",
-                 "phone"=>"3123123",
-                 "email"=>""
-             },
-             "card"=>{
-                 "name"=>"Jonathan Garay",
-                 "exp_month"=>"08",
-                 "exp_year"=>"18",
-                 "country"=>"Mexico",
-                 "brand"=>"VISA",
-                 "last4"=>"4111111111111111"}
-         },
-         "previous_attributes"=>{
-             "livemode"=>"",
-             "amount"=>"",
-             "created_at"=>"0",
-             "status"=>"",
-             "currency"=>"",
-             "description"=>"",
-             "reference_id"=>"",
-             "card"=>{
-                 "name"=>"",
-                 "country"=>""
-             }
-         }
-     },
-     "eventable_id"=>"521266d8ab94e9122e0000e3",
-     "eventable_type"=>"Charge",
-     "updated_at"=>"2013-08-19 18:41:29 UTC",
-     "created_at"=>"2013-08-19 18:41:29 UTC",
-     "type"=>"charge.paid"
+    {
+      "id"=>"51CEF9FAF23668B1F4000001",
+      "created_at"=>"1376949736",
+      "livemode"=>"false",
+      "type"=>"charge.paid",
+      "data"=>{
+        "object"=>{
+          "id"=>"51D5EA80DB49596AA9000001",
+          "created_at"=>"1376949731",
+          "amount"=>"10000",
+          "fee"=>"310",
+          "currency"=>"MXN",
+          "status"=>"paid",
+          "livemode"=>"false",
+          "description"=>"E-Book: Les Miserables",
+          "error"=>"",
+          "error_message"=>"",
+          "dispute"=>"",
+          "card"=>{
+            "last4"=>"1111",
+            "name"=>"Arturo Octavio Ortiz"
+          }
+        },
+        "previous_attributes"=>
+        {
+          "status"=>"payment_pending"
+        }
+      }
     }
   }
 
