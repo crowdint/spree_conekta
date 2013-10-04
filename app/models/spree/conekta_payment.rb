f7c697dcc9edc2b550597d3e2de5710b22f9a7b1
Fernando Cuauhtemoc Barajas Chavez <fernando.barajas@crowdint.com>
2013-10-04 11:38:27 -0500

    [#58249150] Spree Conekta: Add actions to payment sources

--- app/models/spree/conekta_payment.rb
+++ app/models/spree/conekta_payment.rb
@@ -1,4 +1,12 @@
 class Spree::ConektaPayment < ActiveRecord::Base
   attr_accessible :payment_type, :first_name, :last_name
   has_many :payments, as: :source
+
+  def actions
+    %w{capture}
+  end
+
+  def can_capture?(payment)
+    ['checkout', 'pending'].include?(payment.state)
+  end
 end
