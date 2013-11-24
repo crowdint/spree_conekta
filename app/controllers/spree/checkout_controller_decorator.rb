41e515cf362102c5c55beff8a095c96579cb0451
Marco Medina <mmedinalr@gmail.com>
2013-11-23 23:52:39 -0600

    Remove redirection for card payment source

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -1,7 +1,12 @@
 module Spree
   CheckoutController.class_eval do
     def completion_route
-      @order.payments.last.payment_method.class == Spree::BillingIntegration::Conekta ? conekta_payment_path(@order) : spree.order_path(@order)
+      payment_method = @order.payments.last.payment_method
+      if payment_method.class == Spree::BillingIntegration::Conekta || !payment_method.preferred_source_method == 'card'
+        conekta_payment_path(@order)
+      else
+        spree.order_path(@order)
+      end
     end
   end
 end
