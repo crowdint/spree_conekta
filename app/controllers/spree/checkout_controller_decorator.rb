d9d5ef78773de3017caf8431458318b9bff74fe3
Steven Barragan <steven.barragan@crowdint.com>
2014-01-30 10:42:08 -0600

    Refactor completion_route in ChekoutController

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -1,12 +1,17 @@
 module Spree
   CheckoutController.class_eval do
     def completion_route
-      payment_method = @order.payments.last.payment_method
-      if [Spree::BillingIntegration::Conekta::Bank, Spree::BillingIntegration::Conekta::Cash].include? payment_method.class
-        conekta_payment_path(@order)
+      if @order.payments.present? && conekta_payment?(@order.payments.last)
+         conekta_payment_path(@order)
       else
         spree.order_path(@order)
       end
+    end
+
+    private
+
+    def conekta_payment?(payment_method)
+        [Spree::BillingIntegration::Conekta::Bank, Spree::BillingIntegration::Conekta::Cash].include? payment_method.class
     end
   end
 end
