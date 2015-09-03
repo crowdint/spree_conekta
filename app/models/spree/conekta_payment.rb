828863427bbcce522b3b61cb0ab10501f63f6e81
Erick Alvarez <e_adrian90@hotmail.com>
2015-09-02 19:47:07 -0500

    add payment_method_id and user_id as attributes on conekta_payment

--- app/models/spree/conekta_payment.rb
+++ app/models/spree/conekta_payment.rb
@@ -3,7 +3,9 @@ class Spree::ConektaPayment < ActiveRecord::Base
   has_many :payments, as: :source
 
   unless Rails::VERSION::MAJOR == 4
-    attr_accessible :payment_type, :first_name, :last_name
+    attr_accessible :payment_method_id, :payment_type, :first_name, :last_name, :user_id
+  else
+    attr_accessor :payment_method_id, :payment_type, :first_name, :last_name, :user_id
   end
 
   def actions
